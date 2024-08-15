35a71961dca25b848e3064db012c1983700ea1a0
Fagudelo <ferney.certifications@outlook.com>
2024-08-14 21:22:54 -0500

    Se crea el controlador 'StudentsController'

---
+++ StudentPortal/Controllers/StudentsController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StudentPortal.Data;
+using StudentPortal.Models;
+
+namespace StudentPortal.Controllers
+{
+    public class StudentsController(ApplicationDbContext dbContext) : Controller
+    {
+        private readonly ApplicationDbContext _context = dbContext;
+
+        public IActionResult Add()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Add(AddStudentViewModel viewModel)
+        {
+            return View();
+        }
+    }
+}
